9e1ef88434a4e3ffe2c4577d3d9e95166fe523c4
beqakhelidze <khelidzeb@gmail.com>
2021-12-21 23:47:55 +0400

    cleared3

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const http = require('http');
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
+const port = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +10,6 @@ app.get("/", function(req,res) {
     res.render("home");
 })
 
-server.listen(3443, () =>{
+server.listen(port, () =>{
     console.log("ssl connected");
 })
